dab916e637116e58d53496bdb51297280e15b733
JIAN11442 <yikjian11442@gmail.com>
2023-12-08 17:48:18 +0800

    DONE: Connect Mongoose

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,20 @@
+import 'dotenv/config';
 import express from 'express';
+import mongoose from 'mongoose';
+import env from './utils/validateEnv';
 
 const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+mongoose
+  .connect(env.MONGODB_CONNECTION_STRING!)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+    app.listen(env.PORT, () => {
+      console.log(`Server listening on http://localhost:${env.PORT}`);
+    });
+  })
+  .catch(console.error);
